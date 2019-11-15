fa6939b87ea919ffee0ee32a7d5b604887174231
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-15 23:10:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each do |key,value| 
     value.each do |attribute, pigeons_names|
       pigeons_names.each do |name|
-      if name.key?
+      if pigeon_list.has_key?(name)
+        
   
   
   
