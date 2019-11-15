2d15827d7a75ddbe25f35b2490631a4a76a7def0
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-15 23:30:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,16 +7,17 @@ def nyc_pigeon_organizer(data)
       
       pigeons_names.each do |name| #get names
       
-      if pigeon_data.has_key?(name) #check if hash has the name already
+      if pigeon_data.has_key?(name) #check if new hash has the name already
         
         if pigeon_data[name].has_key?(key) # if name, see if it has key
+        
           pigeon_data[name][key] << attribute.to_s #if it has name and key, assign value (brown, new york etc)
         else
           pigeon_data[name][key]=[attribute.to_s] # if no key, assign key and value
         end
         
         else
-        pigeon_data[name]={key=>[attribute.to_s]} # if no name, assign name and key and value
+        pigeon_data[name]={key=>[attribute.to_s]} # if no name, assign name and key and value, basically flipping data
         end
         
       end
