c0c0289e9058ad16e2e927e03204e307fa8254a0
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-15 23:22:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,30 @@
 def nyc_pigeon_organizer(data)
+  
   pigeon_data ={}
+  
   data.each do |key,value| 
     value.each do |attribute, pigeons_names|
+      
       pigeons_names.each do |name|
+      
       if pigeon_data.has_key?(name)
+        
         if pigeon_data[name].has_key?(key)
           pigeon_data[name][key] << attribute.to_s
         else
           pigeon_data[name][key]=[attribute.to_s]
         end
-  pigeon_data[name]={key=>[attribute.to_s]
+        
+        else
+        pigeon_data[name]={key=>[attribute.to_s]
         end
+        
       end
+      
+     end
+   
    end
-  end
   
+  end
+pigeon_data
 end
